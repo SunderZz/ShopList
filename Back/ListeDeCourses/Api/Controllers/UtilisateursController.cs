dd77337f2e938ef185d8517216f3ac76ad4d852f
SunderZz <samuelgillespro@hotmail.com>
2025-09-21 18:59:51 +0200

    Modify Controller #2

--- Back/ListeDeCourses/Api/Controllers/UtilisateursController.cs
+++ Back/ListeDeCourses/Api/Controllers/UtilisateursController.cs
@@ -1,6 +1,7 @@
 ﻿using ListeDeCourses.Api.DTOs;
 using ListeDeCourses.Api.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ListeDeCourses.Api.Controllers;
 
@@ -9,5 +10,15 @@ public class UtilisateursController
   : BaseController<UtilisateurReadDto, UtilisateurCreateDto, UtilisateurUpdateDto>
 {
     public UtilisateursController(UtilisateurService service) : base(service) { }
+
     protected override string GetId(UtilisateurReadDto dto) => dto.Id;
+
+
+    [HttpPost("register")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register([FromBody] UtilisateurCreateDto dto, CancellationToken ct)
+    {
+        var created = await Service.CreateAsync(dto, ct);
+        return CreatedAtAction(nameof(GetById), new { id = GetId(created) }, created);
+    }
 }
